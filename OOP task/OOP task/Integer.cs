8ca99963cabfc269c0175a85d864f88da4de6def
Aurelijus Mėlinis <aurelijus.melinis@ktu.edu>
2021-01-16 17:19:22 +0200

    OOP TASK

---
+++ OOP task/OOP task/Integer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_task
+{
+    class Integer : INumber
+    {
+        private int _integer;
+
+        public Integer(int nr)
+        {
+            _integer = nr;
+        }
+
+        public void DoTasks(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void Filter()
+        {
+            if (_integer % 3 == 0 && _integer % 5 == 0)
+            {
+                DoTasks("SneakyBox");
+            }
+            else if (_integer % 3 == 0)
+            {
+                DoTasks("Sneaky");
+            }
+            else if (_integer % 5 == 0)
+            {
+                DoTasks("Box");
+            }
+            else
+            {
+                DoTasks(_integer.ToString());
+            }
+        }
+    }
+}
